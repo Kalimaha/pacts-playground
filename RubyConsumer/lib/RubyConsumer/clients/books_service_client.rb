8089b7e78161ebef3fe7d8d71892c966ad05bacd
Kalimaha <guido.barbaglia@gmail.com>
2017-04-22 17:21:40 +1000

    Create new book pact.

--- RubyConsumer/lib/RubyConsumer/clients/books_service_client.rb
+++ RubyConsumer/lib/RubyConsumer/clients/books_service_client.rb
@@ -8,7 +8,16 @@ module Clients
     def get_book(id)
       title = JSON.parse(self.class.get("/books/#{id}").body)['title']
 
-      Book.new(title)
+      Book.new(id, title)
+    end
+
+    def create_book(title)
+      payload   = { 'title': title }.to_json
+      response  = JSON.parse(self.class.post("/books", :body => payload).body)
+      id        = response['id']
+      title     = response['title']
+
+      Book.new(id, title)
     end
   end
 end
